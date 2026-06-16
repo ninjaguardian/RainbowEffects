d2b0b39974222ddafe8aa89563b9379750a1b35b
ninjaguardian <110734253+ninjaguardian@users.noreply.github.com>
2026-06-15 23:21:15 -0500

    Remove system calls

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
+## v2.0.0
+- Remove system calls in favor of asset bundles
+
 ## v1.0.0
 - Initial release
--- RainbowGuard/RainbowGuard.cs
+++ RainbowGuard/RainbowGuard.cs
@@ -1,14 +1,9 @@
-﻿using MelonLoader;
-using MelonLoader.NativeUtils;
+﻿using System;
+using MelonLoader;
 using MelonLoader.Utils;
 using RainbowGuard;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 using UnityEngine;
-using static RainbowGuard.DXInterop;
 
 #region Assemblies
 [assembly: MelonInfo(typeof(RainbowGuard.RainbowGuard), RainbowGuardModInfo.ModName, RainbowGuardModInfo.ModVersion, "ninjaguardian", "https://thunderstore.io/c/rumble/p/ninjaguardian/RainbowGuard")]
@@ -20,9 +15,6 @@ using static RainbowGuard.DXInterop;
 [assembly: MelonPlatformDomain(MelonPlatformDomainAttribute.CompatibleDomains.IL2CPP)]
 [assembly: VerifyLoaderVersion(RainbowGuardModInfo.MLVersion, true)]
 #endregion
-
-// TODO: linux (maybe works already)
-// TODO: instead of hooks, find what corresponds to _Color
 
 namespace RainbowGuard
 {
@@ -39,7 +31,7 @@ namespace RainbowGuard
         /// <summary>
         /// Mod version.
         /// </summary>
-        public const string ModVersion = "1.0.0";
+        public const string ModVersion = "2.0.0";
         /// <summary>
         /// MelonLoader Version.
         /// </summary>
@@ -52,408 +44,33 @@ namespace RainbowGuard
     /// </summary>
     public class RainbowGuard : MelonMod
     {
-        private bool _didLoad;
+        private static readonly int ShaderID = Shader.PropertyToID("rainbowGuard");
+        private const float Phase120 = (float)(2d * Math.PI / 3d);
+        private const float Phase240 = (float)(4d * Math.PI / 3d);
 
         /// <inheritdoc/>
-        public override void OnEarlyInitializeMelon() {
-            if (_didLoad)
-                return;
+        public override void OnLateInitializeMelon()
+        {
+            Shader rainbowGuard = RumbleModdingAPI.RMAPI.AssetBundles.LoadAssetFromFile<Shader>(
+                Path.Combine(MelonEnvironment.UserDataDirectory, RainbowGuardModInfo.ModName, "rainbowGuard"),
+                "guard.shader"
+            );
+            rainbowGuard.hideFlags = HideFlags.HideAndDontSave;
 
-            // TODO: what about d3d12?
-            if (SystemInfo.graphicsDeviceType != UnityEngine.Rendering.GraphicsDeviceType.Direct3D11)
-            {
-                MelonLogger.Error("This mod only works on Direct3D11");
-                return;
-            }
-
-            D3D11Output? output = Locate();
-            if (output == null)
-            {
-                MelonLogger.Error("Could not generate D3D11 device");
-                return;
-            }
-
-            using (output)
-            {
-                CreatePixelShaderStore.Init(output);
-                HookCB.Init(output);
-                HookShader.Init(output);
-            }
-
-            _didLoad = true;
+            foreach (Material mat in Resources.FindObjectsOfTypeAll<Material>())
+                if (mat?.name == "Hidden/VFX/Guardstone VFX/System/Output Particle Shader Graph Quad - Unlit")
+                    mat.shader = rainbowGuard;
         }
 
         /// <inheritdoc/>
-        public override void OnDeinitializeMelon()
+        public override void OnUpdate()
         {
-            if (!_didLoad)
-                return;
-
-            CreatePixelShaderStore.DeInit();
-            HookCB.DeInit();
-            HookShader.DeInit();
-        }
-    }
-
-    internal static class HookShader
-    {
-        private static NativeHook<PSSetShader>? _hookInstance;
-
-        internal static readonly object Lock = new();
-        internal static readonly Dictionary<IntPtr, IntPtr> CurrentPsByContext = new();
-        internal const string Match = "Hidden/VFX/Guardstone VFX/System/Output Particle Shader Graph Quad - Unlit";
-
-        internal static void Init(D3D11Output output)
-        {
-            if (_hookInstance != null)
-                return;
-
-            if (output.Context == IntPtr.Zero)
-            {
-                MelonLogger.Error("Could not generate D3D11 context methods");
-                return;
-            }
-
-            IntPtr setShader = GetVTableEntry(output.Context, 9);
-            if (setShader == IntPtr.Zero)
-            {
-                MelonLogger.Error("Failed to get PSSetShader");
-                return;
-            }
-
-            IntPtr detour;
-            unsafe
-            {
-                detour = (IntPtr)(delegate* unmanaged[Stdcall]<IntPtr, IntPtr, IntPtr*, uint, void>)&Hook;
-            }
-            _hookInstance = new NativeHook<PSSetShader>(setShader, detour);
-            _hookInstance.Attach();
-        }
-
-        internal static void DeInit()
-        {
-            _hookInstance?.Detach();
-            _hookInstance = null;
-        }
-
-        [UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvStdcall) })]
-        [MethodImpl(MethodImplOptions.AggressiveOptimization)]
-        private static unsafe void Hook(IntPtr context, IntPtr pPixelShader, IntPtr* ppClassInstances, uint numClassInstances)
-        {
-            _hookInstance!.Trampoline(
-                context,
-                pPixelShader,
-                ppClassInstances,
-                numClassInstances
-            );
-
-            lock (Lock)
-                CurrentPsByContext[context] = pPixelShader;
-        }
-    }
-
-    internal static class HookCB
-    {
-        private static NativeHook<PSSetConstantBuffers>? _hookInstance;
-        private const string BufferName = "ConstantBuffer-733-1632";
-        private const string LivBufferName = "ConstantBuffer-733-2128";
-
-        internal static void Init(D3D11Output output)
-        {
-            if (_hookInstance != null)
-                return;
-
-            if (output.Context == IntPtr.Zero)
-            {
-                MelonLogger.Error("Could not generate D3D11 context methods");
-                return;
-            }
-
-            IntPtr setConstantBuffers = GetVTableEntry(output.Context, 16);
-            if (setConstantBuffers == IntPtr.Zero)
-            {
-                MelonLogger.Error("Failed to get PSSetConstantBuffers");
-                return;
-            }
-
-            IntPtr detour;
-            unsafe
-            {
-                detour = (IntPtr)(delegate* unmanaged[Stdcall]<IntPtr, uint, uint, IntPtr*, void>)&Hook;
-            }
-            _hookInstance = new NativeHook<PSSetConstantBuffers>(setConstantBuffers, detour);
-            _hookInstance.Attach();
-        }
-
-        internal static void DeInit()
-        {
-            _hookInstance?.Detach();
-            _hookInstance = null;
-        }
-
-        [UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvStdcall) })]
-        [MethodImpl(MethodImplOptions.AggressiveOptimization)]
-        private static unsafe void Hook(IntPtr context, uint startSlot, uint numBuffers, IntPtr* ppConstantBuffers)
-        {
-            IntPtr currentPs;
-            lock (HookShader.Lock)
-                HookShader.CurrentPsByContext.TryGetValue(context, out currentPs);
-            if (currentPs == IntPtr.Zero || GetDebugName(currentPs) != HookShader.Match)
-            {
-                _hookInstance!.Trampoline(
-                    context,
-                    startSlot,
-                    numBuffers,
-                    ppConstantBuffers
-                );
-                return;
-            }
-
-            IntPtr mapPtr = GetVTableEntry(context, 14);
-            if (mapPtr == IntPtr.Zero)
-            {
-                MelonLogger.Error("Failed to get Map");
-                _hookInstance!.Trampoline(
-                    context,
-                    startSlot,
-                    numBuffers,
-                    ppConstantBuffers
-                );
-                return;
-            }
-            var map = (delegate* unmanaged[Stdcall]<IntPtr, IntPtr, uint, D3D11_MAP, D3D11_MAP_FLAG, out D3D11_MAPPED_SUBRESOURCE, HResult>)mapPtr;
-
-            IntPtr unmapPtr = GetVTableEntry(context, 15);
-            if (unmapPtr == IntPtr.Zero)
-            {
-                MelonLogger.Error("Failed to get Unmap");
-                _hookInstance!.Trampoline(
-                    context,
-                    startSlot,
-                    numBuffers,
-                    ppConstantBuffers
-                );
-                return;
-            }
-            var unmap = (delegate* unmanaged[Stdcall]<IntPtr, IntPtr, uint, void>)unmapPtr;
-
-            for (uint i = 0; i < numBuffers; i++)
-            {
-                IntPtr resource = ppConstantBuffers[i];
-                string name = GetDebugName(resource);
-                if (name != BufferName && name != LivBufferName)
-                    continue;
-
-                // TODO: instead of changing potentially used buffer slots or overwriting in use data, find a way to create a dedicated buffer
-                HResult err = map(context, resource, 0, D3D11_MAP.D3D11_MAP_WRITE_NO_OVERWRITE, 0, out D3D11_MAPPED_SUBRESOURCE pMappedResource);
-                if (err != HResult.S_OK)
-                {
-                    MelonLogger.Error($"Could not map buffer {name} ({err})");
-                    continue;
-                }
-
-                float* f = (float*)pMappedResource.pData;
-                double t = Environment.TickCount * 0.0005;
-
-                f[12] = (float)(Math.Sin(t * 2.0) * 0.5 + 0.5);
-                f[13] = (float)(Math.Sin(t * 2.0 + 2.094) * 0.5 + 0.5);
-                f[14] = (float)(Math.Sin(t * 2.0 + 4.188) * 0.5 + 0.5);
-
-                unmap(context, resource, 0);
-            }
-
-            _hookInstance!.Trampoline(
-                context,
-                startSlot,
-                numBuffers,
-                ppConstantBuffers
-            );
-        }
-    }
-
-    internal static class CreatePixelShaderStore
-    {
-        private static NativeHook<CreatePixelShader>? _hookInstance;
-        // ReSharper disable StringLiteralTypo
-        private static readonly byte[] MainMatchShader = File.ReadAllBytes(Path.Combine(MelonEnvironment.UserDataDirectory, RainbowGuardModInfo.ModName, "main.match.dxbc"));
-        private static readonly byte[] MainNewShader = File.ReadAllBytes(Path.Combine(MelonEnvironment.UserDataDirectory, RainbowGuardModInfo.ModName, "main.new.dxbc"));
-        private static readonly byte[] LivMatchShader = File.ReadAllBytes(Path.Combine(MelonEnvironment.UserDataDirectory, RainbowGuardModInfo.ModName, "liv.match.dxbc"));
-        private static readonly byte[] LivNewShader = File.ReadAllBytes(Path.Combine(MelonEnvironment.UserDataDirectory, RainbowGuardModInfo.ModName, "liv.new.dxbc"));
-        // ReSharper restore StringLiteralTypo
-
-        internal static void Init(D3D11Output output)
-        {
-            if (_hookInstance != null)
-                return;
-
-            if (output.Device == IntPtr.Zero)
-            {
-                MelonLogger.Error("Could not generate D3D11 device methods");
-                return;
-            }
-
-            IntPtr createPixelShader = GetVTableEntry(output.Device, 15);
-            if (createPixelShader == IntPtr.Zero)
-            {
-                MelonLogger.Error("Failed to get CreatePixelShader");
-                return;
-            }
-
-            IntPtr detour;
-            unsafe
-            {
-                detour = (IntPtr)(delegate* unmanaged[Stdcall]<IntPtr, void*, nuint, IntPtr, IntPtr*, HResult>)&Hook;
-            }
-            _hookInstance = new NativeHook<CreatePixelShader>(createPixelShader, detour);
-            _hookInstance.Attach();
-        }
-
-        internal static void DeInit()
-        {
-            _hookInstance?.Detach();
-            _hookInstance = null;
-        }
-
-#if PRINT_CB
-        [MethodImpl(MethodImplOptions.AggressiveOptimization)]
-        private static unsafe void PrintConstantBuffers(void* shaderBytecode, nuint bytecodeLength)
-        {
-            IntPtr pReflection = IntPtr.Zero;
-            try
-            {
-                HResult err = D3DReflect(shaderBytecode, bytecodeLength, ref Rrid.IID_ID3D11ShaderReflection, out pReflection);
-                if (err != HResult.S_OK)
-                {
-                    MelonLogger.Error($"Failed to reflect shader {err}");
-                    return;
-                }
-                if (pReflection == IntPtr.Zero)
-                {
-                    MelonLogger.Error("Failed to reflect shader");
-                    return;
-                }
-
-                IntPtr shaderDescPtr = GetVTableEntry(pReflection, 3);
-                if (shaderDescPtr == IntPtr.Zero)
-                {
-                    MelonLogger.Error("Failed to get shader description function");
-                    return;
-                }
-                var shaderDesc = (delegate* unmanaged[Stdcall]<IntPtr, out D3D11_SHADER_DESC, HResult>)shaderDescPtr;
-
-                err = shaderDesc(pReflection, out D3D11_SHADER_DESC desc);
-                if (err != HResult.S_OK)
-                {
-                    MelonLogger.Error($"Failed to get shader description {err}");
-                    return;
-                }
-
-                IntPtr getConstantBufferByIndexPtr = GetVTableEntry(pReflection, 4);
-                if (getConstantBufferByIndexPtr == IntPtr.Zero)
-                {
-                    MelonLogger.Error("Failed to get GetConstantBufferByIndex");
-                    return;
-                }
-                var getConstantBufferByIndex =
-                    (delegate* unmanaged[Stdcall]<IntPtr, uint, IntPtr>)
-                    getConstantBufferByIndexPtr;
-
-                for (uint i = 0; i < desc.ConstantBuffers; i++)
-                {
-                    IntPtr pCB = getConstantBufferByIndex(pReflection, i);
-                    if (pCB == IntPtr.Zero)
-                    {
-                        MelonLogger.Warning($"Failed to get constant buffer for index {i}");
-                        continue;
-                    }
-
-                    IntPtr bufferDescPtr = GetVTableEntry(pCB, 0);
-                    if (bufferDescPtr == IntPtr.Zero)
-                    {
-                        MelonLogger.Error($"Failed to get buffer description function for index {i}");
-                        continue;
-                    }
-                    var bufferDesc = (delegate* unmanaged[Stdcall]<IntPtr, out D3D11_SHADER_BUFFER_DESC, HResult>)bufferDescPtr;
-
-                    err = bufferDesc(pCB, out D3D11_SHADER_BUFFER_DESC cbDesc);
-                    if (err != HResult.S_OK)
-                    {
-                        MelonLogger.Warning($"Failed to get constant buffer description for index {i}, {err}");
-                        continue;
-                    }
-
-                    string? name = cbDesc.Name != IntPtr.Zero
-                        ? Marshal.PtrToStringAnsi(cbDesc.Name)
-                        : "<null>";
-
-                    MelonLogger.Msg($"Constant Buffer {i}: {name}, Size: {cbDesc.Size}");
-                }
-            }
-            finally
-            {
-                if (pReflection != IntPtr.Zero)
-                    Marshal.Release(pReflection);
-            }
-        }
-#endif
-
-        [UnmanagedCallersOnly(CallConvs = new[] { typeof(CallConvStdcall) })]
-        [MethodImpl(MethodImplOptions.AggressiveOptimization)]
-        private static unsafe HResult Hook(
-            IntPtr device,
-            void* shaderBytecode,
-            nuint bytecodeLength,
-            IntPtr classLinkage,
-            IntPtr* pixelShader
-        )
-        {
-#if PRINT_CB
-            PrintConstantBuffers(shaderBytecode, bytecodeLength);
-#endif
-
-            if
-            (
-                bytecodeLength == (nuint)MainMatchShader.Length &&
-                new ReadOnlySpan<byte>(shaderBytecode, MainMatchShader.Length).SequenceEqual(MainMatchShader)
-            )
-            {
-                fixed (byte* pNewShader = MainNewShader)
-                {
-                    return _hookInstance!.Trampoline(
-                        device,
-                        pNewShader,
-                        (nuint)MainNewShader.Length,
-                        classLinkage,
-                        pixelShader
-                    );
-                }
-            }
-
-            if
-            (
-                bytecodeLength == (nuint)LivMatchShader.Length &&
-                new ReadOnlySpan<byte>(shaderBytecode, LivMatchShader.Length).SequenceEqual(LivMatchShader)
-            )
-            {
-                fixed (byte* pNewShader = LivNewShader)
-                {
-                    return _hookInstance!.Trampoline(
-                        device,
-                        pNewShader,
-                        (nuint)LivNewShader.Length,
-                        classLinkage,
-                        pixelShader
-                    );
-                }
-            }
-
-            return _hookInstance!.Trampoline(
-                device,
-                shaderBytecode,
-                bytecodeLength,
-                classLinkage,
-                pixelShader
-            );
+            Shader.SetGlobalVector(ShaderID, new Vector4(
+                Mathf.Sin(Time.time) * 0.5f + 0.5f,
+                Mathf.Sin(Time.time + Phase120) * 0.5f + 0.5f,
+                Mathf.Sin(Time.time + Phase240) * 0.5f + 0.5f,
+                0
+            ));
         }
     }
 }
